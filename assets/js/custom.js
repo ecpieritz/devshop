c6a5540554228a395ac9f46f25b06b4194bfa47e
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2022-04-24 14:50:05 -0300

    Added form scripts

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -23,3 +23,48 @@ $(".client_owl-carousel").owlCarousel({
         }
     }
 });
+
+// GET THE INFORMATION THAT IS INSIDE THE FIELD
+
+const btnSalvar         = document.querySelector(".salvar")
+const ptitulo            = document.querySelector(".ptitulo")
+const pdesc           = document.querySelector(".pdesc")
+const pvalor           = document.querySelector(".pvalor")
+const pimg           = document.querySelector(".pimg")
+const tagResultado       = document.querySelector(".resultado p")
+const popUpResultado     = document.querySelector(".resultado")
+const btnFechar          = document.querySelector(".resultado")
+
+//GETTING DATA FROM THE FORM
+function pegarDadosFormulario(){
+    
+    //RECOVERING FIELD VALUE (TEXTAREA)
+    
+    const titulo  = ptitulo.value
+    mostrarResultado(titulo)
+
+    ptitulo.value = ""
+    pdesc.value = ""
+    pvalor.value = ""
+    pimg.value = ""
+    
+}
+
+//INTERCEPTING THE CLICK / RUNNING FUNCTION
+btnSalvar.addEventListener("click", pegarDadosFormulario)
+
+
+function mostrarResultado(titulo){
+
+    tagResultado.innerText = `${titulo} salvo!`
+    popUpResultado.classList.add("mostrarResultado")
+    
+}
+
+
+function fecharPopUp(){
+
+    popUpResultado.classList.remove("mostrarResultado")
+
+}
+btnFechar.addEventListener("click", fecharPopUp)
